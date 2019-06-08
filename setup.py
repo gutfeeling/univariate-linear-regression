fcfe178685124b258d8dbed49aedef9b48c45331
Dibya Chakravorty <dibyachakravorty@gmail.com>
2019-06-08 19:11:17 +0200

    Add full author name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(name="univariate_linear_regression",
       version="0.1.0",
       description="Univariate linear regression of housing price against housing area",
-      author="Dibya",
+      author="Dibya Chakravorty",
       packages=find_packages("src"),
       package_dir={"": "src"},
       author_email="dibyachakravorty@gmail.com",
