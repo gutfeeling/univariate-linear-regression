e5f39646efa79b5c67d13ba9ad2aa34cccf7a02c
Dibya Chakravorty <dibyachakravorty@gmail.com>
2019-06-15 21:07:59 +0200

    Add test for preprocessing function

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -1,6 +1,4 @@
-import argparse
-
-from .preprocessing_helpers import row_to_list, convert_to_int
+from data.preprocessing_helpers import row_to_list, convert_to_int
 
 
 def preprocess(input_file_path, output_file_path):
@@ -11,22 +9,8 @@ def preprocess(input_file_path, output_file_path):
             row_as_list = row_to_list(row)
             if row_as_list is None:
                 continue
-            try:
-                area = convert_to_int(row_as_list[0])
-            except ValueError:
-                continue
-            try:
-                price = convert_to_int(row_as_list[1])
-            except ValueError:
-                continue
+            area = convert_to_int(row_as_list[0])
+            price = convert_to_int(row_as_list[1])
             if area is None or price is None:
                 continue
             output_file.write("{0}\t{1}\n".format(area, price))
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input-file-path", "-i", type=str)
-    parser.add_argument("--output-file-path", "-o", type=str)
-    args = parser.parse_args()
-    preprocess(**vars(args))
